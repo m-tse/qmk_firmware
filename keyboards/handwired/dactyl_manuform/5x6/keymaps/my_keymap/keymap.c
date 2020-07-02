2714c27d91dfda39758f7b371f8bb54119629fed
m <1315979+mattse@users.noreply.github.com>
2020-07-02 18:43:04 -0400

    Clean up formatting of keymap.c

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -1,29 +1,7 @@
-/* A standard layout for the Dactyl Manuform 5x6 Keyboard */ 
-
 #include QMK_KEYBOARD_H
-
-
-#define _QWERTY 0
-#define _NAV 1
-#define _NUMPAD 2
-#define _NUM_SYMBOL 3
 
 enum custom_keycodes {
     LAYER_CLEAR = SAFE_RANGE,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case LAYER_CLEAR:
-        if (record->event.pressed) {
-            // when keycode LAYER_CLEAR is pressed
-            layer_clear();
-        } else {
-            // when keycode LAYER_CLEAR is released
-        }
-        break;
-    }
-    return true;
 };
 
 // Tap Dance declarations
@@ -35,19 +13,8 @@ enum {
     T5,
 };
 
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for the number, twice for the F-key.
-    [T1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F1),
-    [T2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F2),
-    [T3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F3),
-    [T4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
-    [T5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [_QWERTY] = LAYOUT_5x6(
+  [0] = LAYOUT_5x6(
         KC_GRV, TD(T1), TD(T2), TD(T3), TD(T4), TD(T5),                 KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSLS,
 LSFT_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
@@ -58,7 +25,7 @@ LSFT_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   
                                         TO(1),  OSL(3),                 KC_CAPS,TO(2)
   ),
 
-  [_NAV] = LAYOUT_5x6(
+  [1] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
         _______,KC_HOME,_______,KC_UP,  _______,KC_PGUP,                _______,_______,_______,_______,_______,_______,
         _______,KC_END, KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,                _______,_______,_______,_______,_______,_______,
@@ -67,10 +34,9 @@ LSFT_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   
                                         _______,_______,                _______,_______,
                                         _______,_______,                _______,_______,
                                         _______,_______,                _______,_______
-
   ),
 
-  [_NUMPAD] = LAYOUT_5x6(
+  [2] = LAYOUT_5x6(
         _______,_______,_______,KC_PSLS,KC_PAST,KC_PMNS,                _______,_______,KC_PSLS,KC_PAST,KC_PMNS,_______,
         _______,_______,KC_KP_7,KC_KP_8,KC_KP_9,KC_PPLS,                _______,KC_KP_7,KC_KP_8,KC_KP_9,KC_PPLS,_______,
         _______,_______,KC_KP_4,KC_KP_5,KC_KP_6,KC_PPLS,                _______,KC_KP_4,KC_KP_5,KC_KP_6,KC_PPLS,_______,
@@ -80,7 +46,8 @@ LSFT_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                   KC_H,   
                                         _______,_______,                _______,_______,
                                         _______,_______,                _______,_______
   ),
-  [_NUM_SYMBOL] = LAYOUT_5x6(
+
+  [3] = LAYOUT_5x6(
         _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
         _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                  KC_F7,  KC_F8,  KC_F9,  KC_MINS,KC_EQL, _______,
         _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,
@@ -102,3 +69,27 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
 }
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for the number, twice for the F-key.
+    [T1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F1),
+    [T2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F2),
+    [T3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F3),
+    [T4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
+    [T5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case LAYER_CLEAR:
+        if (record->event.pressed) {
+            // when keycode LAYER_CLEAR is pressed
+            layer_clear();
+        } else {
+            // when keycode LAYER_CLEAR is released
+        }
+        break;
+    }
+    return true;
+};
