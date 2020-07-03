c46d8283de0504759be275a9f0883ba99adc304a
m <1315979+mattse@users.noreply.github.com>
2020-07-03 02:44:34 -0400

    Add comments to flash and compile scripts

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
+# Just compile the keymap to see if it's working.
 qmk compile -kb handwired/dactyl_manuform/5x6 -km my_keymap
--- flash_dfu.sh
+++ flash_dfu.sh
@@ -1,1 +1,2 @@
+# Compile and flash the current key configuration. 
 make handwired/dactyl_manuform/5x6:my_keymap:dfu
