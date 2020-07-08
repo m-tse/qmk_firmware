ca5d5c067bbbfa57c712f7389827b6154e0cfcc5
m <1315979+mattse@users.noreply.github.com>
2020-07-07 21:05:28 -0400

    Update comment for number/symbol layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |       |       |       |       |       |           |       |       |       |       |       |
     ),
 
-    // Aspirational number/symbol layer.
+    // Number/symbol layer, provides access to obscure keys closer to the home row.
     [3] = LAYOUT_5x6(
     // |       |       |       |       |       |           |       |       |       |       |       |
         KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,      KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
