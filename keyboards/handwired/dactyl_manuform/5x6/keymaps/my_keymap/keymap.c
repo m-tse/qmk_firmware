78eed6b679d3e01632a3c276c3e2b428cda953ab
m <1315979+mattse@users.noreply.github.com>
2021-08-03 20:38:24 -0400

    Several Tweaks:
    - Remove game layer key, I almost never use it, just gets annoying
    - Move Caps Lock to the layer clear spot
    - Unbind bottom right key

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -32,12 +32,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         L_FUNC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_QUOT,
         KC_LSFT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_RSFT,
-        KC_LCTL,WIN_Z,  KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_CAPS,
+        KC_LCTL,WIN_Z,  KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,_______,
     // |       |       |       |       |       |           |       |       |       |       |       |
                         KC_LEFT,KC_RIGHT,                                   KC_DOWN,KC_UP,
                                         KC_LALT,KC_SPC,     KC_BSPC,KC_ENT,
-                                        KC_UP,  L_SYM,      L_SYM,  LYR_CLR,
-                                        KC_DOWN,L_GAME,     KC_VOLD,KC_VOLU
+                                        KC_UP,  L_SYM,      L_SYM,  KC_CAPS,
+                                        KC_DOWN,_______,    KC_VOLD,KC_VOLU
     // |       |       |       |       |       |           |       |       |       |       |       |
     ),
 
