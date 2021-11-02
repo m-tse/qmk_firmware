65d7e489c4813490eaa25892d4ee5816049c14f6
Matthew Tse <1315979+m-tse@users.noreply.github.com>
2021-11-02 08:06:31 -0400

    Add docker build script

---
+++ compile_docker.sh
@@ -0,0 +1,4 @@
+# Script to compile using docker
+# First, install docker mac, the application
+# git pull this repo, including submodules 
+./util/docker_build.sh handwired/dactyl_manuform/5x6:my_keymap
