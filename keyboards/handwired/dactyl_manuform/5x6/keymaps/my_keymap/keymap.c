117bd8041a38af6b096ab6ae73bfacd31e1e46a8
Matthew Tse <matt.s.tse@gmail.com>
2023-04-06 01:38:01 -0400

    Better use of home/end, pagedown/pageup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_RSFT,
         KC_LCTL,WIN_Z,  KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_DEL,
     // |       |       |       |       |       |           |       |       |       |       |       |
-                        KC_LEFT,KC_RIGHT,                                   KC_DOWN,KC_UP,
+                        KC_LEFT,KC_RIGHT,                                   KC_HOME,KC_END,
                                         KC_LALT,KC_SPC,     KC_BSPC,KC_ENT,
                                         KC_UP,  L_SYM,      L_SYM,  KC_CAPS,
                                         KC_DOWN,_______,    KC_VOLD,KC_VOLU
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |       |       |       |       |       |           |       |       |       |       |       |
                         _______,_______,                                    _______,_______,
                                         _______,_______,    _______,_______,
-                                        KC_HOME,_______,    _______,_______,
-                                        KC_END, _______,    _______,_______
+                                        KC_PGUP,_______,    _______,_______,
+                                        KC_PGDN, _______,    _______,_______
     // |       |       |       |       |       |           |       |       |       |       |       |
     ),
 
