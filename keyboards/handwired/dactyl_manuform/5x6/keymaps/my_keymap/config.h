7a4bc0b5f36cd62cf4ab7bc9b07ea64ce4bc2f91
Matthew Tse <matt.s.tse@gmail.com>
2023-04-10 19:01:50 -0400

    Try double tap LCTL for META instead of Z being dual functioned

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/config.h
@@ -19,15 +19,5 @@
 // Default diode direction
 #define DIODE_DIRECTION COL2ROW
 
-// See https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
-// Turning this on would prevent a quick `'s` from resulting in `S` if the right quote key was a shift mod tap key.
-// However, it means I need to properly hold shift and the key to be shifted for longer than
-// the tapping term, which is worse. So this is disabled for now.
-// #define IGNORE_MOD_TAP_INTERRUPT
-// See explanation in `get_ignore_mod_tap_interrupt` of `keymap.c`
-#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
-
-// Tapping term is used for many things, e.g.
-// - how fast I need to double tap the number keys to get an F key
-// - for mod-shifts, how long they need to be held to be considered a hold rather than a tap.
+// Tapping term is used for how fast I must double tap L_CTL to get L_META
 #define TAPPING_TERM 200
--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -4,11 +4,21 @@ enum custom_keycodes {
     SLEEP = SAFE_RANGE,
 };
 
+// Tap Dance declarations
+enum {
+    TD_CTL_GUI,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for CTRL, twice for META/GUI
+    [TD_CTL_GUI] = ACTION_TAP_DANCE_DOUBLE(KC_LCTRL, KC_LGUI),
+};
+
 #define BASE 0
 #define NUMBER_SYMBOL 1
 #define FUNCTION 2
 
-#define WIN_Z WIN_T(KC_Z)
+// #define WIN_Z WIN_T(KC_Z)
 #define L_SYM MO(NUMBER_SYMBOL)
 #define L_FUNC LT(FUNCTION, KC_ESC)
 
@@ -25,12 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //   extra thumb cluster key and then hit a number key for desktop swapping, especially since it would require
     //   hopping over the space key. So I added a layer tap on top of the tilde key. Tap it for backtick/tilde, hold it
     //   to go to the F key layer.
+    // - I had Z be a hold-for-meta key for awhile, but got too annoyed with Z typos. Now I made LCTL into META when
+    //   double tapped.
     [BASE] = LAYOUT_5x6(
     // |       |       |       |       |       |           |       |       |       |       |       |
         L_FUNC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_QUOT,
         KC_LSFT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_RSFT,
-        KC_LCTL,WIN_Z,  KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_DEL,
+ TD(TD_CTL_GUI),KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_DEL,
     // |       |       |       |       |       |           |       |       |       |       |       |
                         KC_LEFT,KC_RIGHT,                                   KC_HOME,KC_END,
                                         KC_LALT,KC_SPC,     KC_BSPC,KC_ENT,
@@ -69,17 +81,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |       |       |       |       |       |           |       |       |       |       |       |
     ),
 };
-
-// I want shift to work quickly, however I want the meta mod tap key to work a bit slower,
-// which allows typing `zsh` without doing a "meta + s" combination.
-bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case WIN_T(KC_Z):
-            return true;
-        default:
-            return false;
-    }
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
