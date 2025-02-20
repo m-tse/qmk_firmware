017c1b6580b2185d68eaf15adb368dbd5d212651
Matthew Tse <matthew@matthewtse.com>
2025-02-19 22:33:27 -0500

    Added a special arrows layer, useful for when I'm using the iris keyboard and don't have arrows

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -7,8 +7,9 @@ enum custom_keycodes {
 #define BASE 0
 #define NUMBER_SYMBOL 1
 #define FUNCTION 2
-
+#define ARROWS 3
 #define L_SYM MO(NUMBER_SYMBOL)
+#define L_ARROWS MO(ARROWS)
 #define L_FUNC LT(FUNCTION, KC_ESC)
 // This is a workaround, where my logitech M720 mouse button is hardcoded to hit LCTL+LGUI+TAB, so I need to make the
 // keyboard do the same combination so I can have both a mouse and keyboard way to use mission control.
@@ -34,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         L_FUNC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_QUOT,
         KC_LSFT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_RSFT,
-        KC_LCTL,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_DEL,
+        KC_LCTL,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,L_ARROWS,
     // |       |       |       |       |       |           |       |       |       |       |       |
                         KC_LEFT,KC_RIGHT,                                   KC_HOME,KC_END,
                                         KC_LALT,KC_SPC,     KC_BSPC,KC_ENT,
@@ -68,6 +69,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // |       |       |       |       |       |           |       |       |       |       |       |
                         _______,_______,                                    _______,_______,
                                        MISS_CTL,_______,    _______,_______,
+                                        _______,_______,    _______,_______,
+                                        _______,_______,    _______,_______
+    // |       |       |       |       |       |           |       |       |       |       |       |
+    ),
+    [ARROWS] = LAYOUT_5x6(
+    // |       |       |       |       |       |           |       |       |       |       |       |
+        _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,
+        _______,_______,_______,KC_UP,  _______,_______,    _______,_______,_______,_______,_______,_______,
+        KC_LSFT,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,    _______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,
+    // |       |       |       |       |       |           |       |       |       |       |       |
+                        _______,_______,                                    _______,_______,
+                                        _______,_______,    _______,_______,
                                         _______,_______,    _______,_______,
                                         _______,_______,    _______,_______
     // |       |       |       |       |       |           |       |       |       |       |       |
