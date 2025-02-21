82fe39f03c16d36d138e522e48f58c494ddd0d0f
Matthew Tse <matthew@matthewtse.com>
2025-02-21 17:28:18 -0500

    Remove home/end bindings, they mess me up, I never use them

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_RSFT,
         KC_LCTL,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,L_ARROWS,
     // |       |       |       |       |       |           |       |       |       |       |       |
-                        KC_LEFT,KC_RIGHT,                                   KC_HOME,KC_END,
+                        KC_LEFT,KC_RIGHT,                                   KC_NO,  KC_NO,
                                         KC_LALT,KC_SPC,     KC_BSPC,KC_ENT,
                                         KC_UP,  L_SYM,      L_SYM,  KC_CAPS,
                                         KC_DOWN,KC_LGUI,    KC_VOLD,KC_VOLU
